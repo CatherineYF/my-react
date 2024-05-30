1fb5df3a10c17c3763b9b11e4f1dd40f96fdc8d3
zhangyifan <zhangyifn@yonyou.com>
2024-05-30 23:13:10 +0800

    some changes

--- src/components/Todos/index.tsx
+++ src/components/Todos/index.tsx
@@ -1,4 +1,6 @@
 import TodoView from "./todoView"
+import { useState } from "react";
+import { observer } from 'mobx-react-lite'
 interface TodoViewStore {
   title: string;
   finished: boolean;
@@ -11,15 +13,17 @@ interface TodosProps{
   todoListStore: TodoListStore;
 }
 function Todos({todoListStore}:TodosProps){
+  let [title,setTitle] = useState("")
   function handleKeyUp(ev:React.KeyboardEvent<HTMLInputElement>){
-    if(ev.keyCode === 13){
+    if(ev.key === 'Enter'){
       todoListStore.addTodo(ev.currentTarget.value)
+      setTitle('')
     }
   }
   return (<div>
-    <input placeholder="请输入待办todo" onKeyUp={handleKeyUp}></input>
-    {todoListStore.todos.map(todo=><TodoView todo={todo}></TodoView>)}
+    <input placeholder="请输入待办todo" onKeyUp={handleKeyUp} value={title} onChange={ev=>setTitle(ev.target.value)}></input>
+    {todoListStore.todos.map((todo,idx)=><TodoView key={idx} todo={todo}></TodoView>)}
   </div>
 )}
 
-export default Todos
+export default observer(Todos)
--- src/stores/todoStore/TodoListStore.js
+++ src/stores/todoStore/TodoListStore.js
@@ -1,13 +1,20 @@
 import TodoViewStore from './TodoViewStore';
+import { action, makeObservable, observable } from 'mobx';
 class TodoListStore {
   todos = [];
   constructor(todos) {
     if(Array.isArray(todos)){
       this.todos = todos;
     }
+    makeObservable(this,{
+      todos:observable,
+      addTodo:action
+    },{
+      autoBind:true
+    })
   }
   addTodo(title){
-    this.todos.push(new TodoViewStore(title))
+    this.todos.push(new TodoViewStore({title}))
   }
 }
 export default TodoListStore;
